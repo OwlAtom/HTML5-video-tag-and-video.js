fdd017c4dbb0d97a9a0a7e69654fa0be004ec491
OwlEats <57601828+OwlEats@users.noreply.github.com>
2020-08-27 11:06:35 +0200

    video on about page, global video.js options, centered play button on video4

--- dahg_files/assets/js/script-about.js
+++ dahg_files/assets/js/script-about.js
@@ -1,3 +1,6 @@
 // JS by Dan Høegh
 // UCN MMD 2020
 
+
+videojs("video3", background);
+videojs("video4", content);
--- dahg_files/assets/js/script.js
+++ dahg_files/assets/js/script.js
@@ -1,3 +1,18 @@
 // JS by Dan Høegh
 // UCN MMD 2020
 
+
+const background = {
+    "controls":false,
+    "autoplay":true,
+    "preload":"auto",
+    "muted":true,
+    "loop":true
+}
+
+const content = {
+    "controls":true,
+    "autoplay":false,
+    "preload":"auto",
+    "muted":false
+}
