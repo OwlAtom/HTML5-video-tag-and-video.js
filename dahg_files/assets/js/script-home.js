329bb571733350d5a342a075b7b9c0957d992f7e
OwlEats <57601828+OwlEats@users.noreply.github.com>
2020-08-27 10:37:58 +0200

    finished video.js on home page

--- dahg_files/assets/js/script-home.js
+++ dahg_files/assets/js/script-home.js
@@ -1,5 +1,16 @@
 // JS by Dan Høegh
 // UCN MMD 2020
 
+let options = {
+    "controls":true,
+    "autoplay":true,
+    "preload":"auto",
+    "muted":true
+}
 
+videojs("video2", options);
 
+videojs("video2").on("ready", function() {
+    this.addClass("my-example");
+    console.log(this.id_ + " is now playing")
+})
